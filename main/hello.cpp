c996237051f45db0d1dc2e04e075b77ea981b85b
nspushkar <131574165+nspushkar@users.noreply.github.com>
2025-03-10 16:27:05 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,9 +1,9 @@
-//File: hello.cpp
+ //File: hello.cpp
 #include <iostream>
 using namespace std;
 
 int main(void) {
-  cout << "Hello, hello check work please World\n";
+  cout << "Hello, hello check work please World edit now please \n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
